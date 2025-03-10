d184f36f31cf9e5583640dc7a1833b35eb587914
Rafi1310 <r.ijssel3@gmail.com>
2025-03-10 13:25:11 +0100

    Login pagina met server.js samen

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app
     .get('/', onhome)
     .listen(8000)
 
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('login', { title: "Loginpagina", message: "Welkom op mijn website" });
+});
+app.listen(8000)
+
 function onhome(req, res) {
     res.send('<h1>Hello World</h1>')
 }
