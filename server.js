f68748b7e2f5b229a6f0e2fae0d6ef3960f54623
Rafi1310 <r.ijssel3@gmail.com>
2025-03-13 13:58:45 +0100

    aanpassing

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.get('/login', (req, res) => {
     res.render('login', { title: "Loginpagina", message: "Welkom op mijn website" });
 });
 
-// Start de server op poort 8000
-app.listen(8000, () => {
-    console.log('Server draait op http://localhost:8000');
-});
 
 // Route voor de registreerpagina
 app.get('/registreren', (req, res) => {
@@ -29,7 +25,6 @@ app.post('/registreren', (req, res) => {
     res.send("Registratie succesvol! (Hier kun je de gegevens opslaan in een database)");
 });
 
-const bcrypt = require('bcrypt');
 
 // Stel EJS in als de template engine
 app.set('view engine', 'ejs');
