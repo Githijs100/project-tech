022b6d70f8adf433a059845b379134adaf1ca7ed
Githijs100 <thijs.krook1@gmail.com>
2025-03-17 11:49:06 +0100

    /registreren route aangepast

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 require('dotenv').config(); // Laad de omgevingsvariabelen
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(uri);
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app = express();
+const uri = process.env.URI;
+const port = 8000;
+
+// Verbinden met MongoDB bij het starten van de server
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Verbonden met MongoDB");
+    } catch (err) {
+        console.error("Kan niet verbinden met MongoDB:", err);
+        process.exit(1);
+    }
+}
+connectDB();
+
+// Middleware om formulierdata te parseren
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/registreren', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.send("Gebruiker geregistreerd met versleuteld wachtwoord!");
+    } catch (err) {
+        res.status(500).send("Fout bij registratie: " + err.message);
+    }
+});
 
 // Stel EJS in als template engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello World</h1>');
 });
 
 app.get('/login', (req, res) => {
