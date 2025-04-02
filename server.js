b59d80c23b80c326d000fdd9e1619ef8dfebc15d
Githijs100 <thijs.krook1@gmail.com>
2025-04-02 16:36:46 +0200

    inloggen werkt

--- server.js
+++ server.js
@@ -74,9 +74,9 @@ app.get('/profiel', async (req, res) => {
 });
 // ✅ Login Route
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ username });
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(401).send("❌ Ongeldige inloggegevens");
         }
