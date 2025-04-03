2da388bfff09e9a8841fef670cd21f2225ff4125
Githijs100 <thijs.krook1@gmail.com>
2025-04-03 14:52:18 +0200

    array weggehaald

--- models/beerModel.js
+++ models/beerModel.js
@@ -4,7 +4,7 @@ const beerSchema = new mongoose.Schema({
     name: String,
     country: String,
     abv: Number,
-    food_pairing: [String],
+    food_pairing: String,
     sub_category_2: String,
     image: String
 });
