1a9967ed4bd7b5294ab82023ce30a0ca8018bf89
Rafi1310 <r.ijssel3@gmail.com>
2025-04-03 20:07:26 +0200

    main fix

--- models/user.js
+++ models/user.js
@@ -4,13 +4,6 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    savedBeers: [{ 
-        sku: String,
-        name: String,
-        brewery: String,
-        abv: String,
-        image: String 
-    }]
     password: { type: String, required: true },
 
     savedBeers: [{
